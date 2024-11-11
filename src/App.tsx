336fbc37609973fce6eccfac3e40dce7c9a84357
jatin <jatinpal4015@gmail.com>
2024-11-11 22:41:39 +0530

    Feature: tailwind and mantine implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import "./App.css";
+import { Button } from '@mantine/core'
+import './App.css'
 
 function App() {
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      overflow: "hidden",
-      height: '100vh'
-    }}>
-      <h1 style={{
-        color: "transparent",
-        background: 'linear-gradient(62deg, #FBAB7E 0%, #F7CE68 100%)',
-        backgroundClip: 'text'
-      }}>Coming soon.....</h1>
-      <span style={{
-        color: "grey",
-
-      }}>Powered by Team JJ.</span>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        overflow: 'hidden',
+        height: '100vh',
+      }}
+    >
+      <h1
+        style={{
+          color: 'transparent',
+          background: 'linear-gradient(62deg, #FBAB7E 0%, #F7CE68 100%)',
+          backgroundClip: 'text',
+        }}
+      >
+        Coming soon.....
+      </h1>
+      <span
+        style={{
+          color: 'grey',
+        }}
+      >
+        Powered by Team JJ.
+      </span>
+      <Button>Hello</Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import '@mantine/core/styles.css';
 import './index.css'
+import { MantineProvider } from '@mantine/core';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <MantineProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </MantineProvider>
+  ,
 )
