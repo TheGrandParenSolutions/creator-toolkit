7342696666575bec7c219ee7a210529d46715247
jatin <jatinpal4015@gmail.com>
2025-02-21 22:52:19 +0530

    fix gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,30 +17,30 @@ export default {
         transform: "transform",
       },
       backgroundImage: {
-        'main-gradient': 'linear-gradient(62deg, #fbab7e 0%, #ffd580 50%, rgb(255, 213, 128) 100%)',
+        'main-gradient': 'linear-gradient(62deg,#fbab7e 0%, #ffd580 50%, #FFC676 100%)',
         'sec-gradient': `linear-gradient(
           0deg,
         rgba(255, 125, 0, 1) 0%,
         rgba(255, 95, 109, 1) 100%
         )`
+    },
+    keyframes: {
+      appear: {
+        "0%": { opacity: "0", transform: "scale(0.8) rotate(-45deg)" },
+        "50%": { opacity: "0.5", transform: "scale(1.1) rotate(20deg)" },
+        "100%": { opacity: "1", transform: "scale(1) rotate(0deg)" },
       },
-      keyframes: {
-        appear: {
-          "0%": { opacity: "0", transform: "scale(0.8) rotate(-45deg)" },
-          "50%": { opacity: "0.5", transform: "scale(1.1) rotate(20deg)" },
-          "100%": { opacity: "1", transform: "scale(1) rotate(0deg)" },
-        },
-        disappear: {
-          "0%": { opacity: "1", transform: "scale(1) rotate(0deg)" },
-          "50%": { opacity: "0.5", transform: "scale(1.1) rotate(20deg)" },
-          "100%": { opacity: "0", transform: "scale(0.8) rotate(-45deg)" },
-        },
-      },
-      animation: {
-        appear: "appear 1s ease-in-out forwards",
-        disappear: "disappear 1s ease-in-out forwards",
+      disappear: {
+        "0%": { opacity: "1", transform: "scale(1) rotate(0deg)" },
+        "50%": { opacity: "0.5", transform: "scale(1.1) rotate(20deg)" },
+        "100%": { opacity: "0", transform: "scale(0.8) rotate(-45deg)" },
       },
     },
+    animation: {
+      appear: "appear 1s ease-in-out forwards",
+      disappear: "disappear 1s ease-in-out forwards",
+    },
   },
-  plugins: [],
+},
+plugins: [],
 }
